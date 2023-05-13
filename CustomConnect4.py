70601f9923d70ebb0c57b043c88c42442c141992
ChazHanda <75233346+ChazHanda@users.noreply.github.com>
2023-05-12 17:08:08 -0700

    Update CustomConnect4.py
    Added AI using the minimax algorithm
    Added randomness to the AI algorithm
    Added more checking and error avoidance to user inputs
    Added limits to board size

--- CustomConnect4.py
+++ CustomConnect4.py
@@ -1,15 +1,42 @@
 #Connect 4 game
+
 import time
+import random
+
+#board dimensions
+MIN_WIDTH = 4
+MAX_WIDTH = 7
+MIN_HEIGHT = 4
+MAX_HEIGHT = 10
+
+#AI score and limitations
+WIN_VALUE = 9999
+LOSE_VALUE = -9999
+DRAW_VALUE = 0
+#TWO_VALUE = 1
+#THREE_VALUE = 10
+MAX_EVAL = -99999
+MIN_EVAL = 99999
+DEFAULT_DEPTH = 8
+
 
 #class containing board state and methods of board manipulation
 class BoardState:
-    def __init__ (self, rows, columns, state=None):
+    def __init__ (self, rows, columns, state=None, depth=None):
         self.row = rows
         self.column = columns
         if state is None:
             self.board = [[0 for x in range(self.column)] for y in range(self.row)]
         else:
             self.board = state
+        if depth is None:
+            self.depth = DEFAULT_DEPTH
+        else:
+            self.depth = depth
+
+    def clearBoard(self):
+        self.board = [[0 for x in range(self.column)] for y in range(self.row)]
+        return
 
     def otherPlayer(self, player):
         if (player == 1):
@@ -17,17 +44,20 @@ class BoardState:
         else:
             return 1
 
+#returns number of columns
     def getColumn(self, sub = None):
         if (sub == None):
             return self.column
-        else:
-            return int( self.column - 1)
-    
+
+#returns number of rows
+    def getRows(self):
+        return self.row
+
     def getBoard(self):
         return self.board
 
     def boardCopy(self):
-        return [row[:] for row in self.board]
+        return BoardState(self.row, self.column,[row[:] for row in self.board])
 
     def printBoard(self):
         for row in range(self.row):
@@ -47,27 +77,29 @@ class BoardState:
             bottomText += str(columnPos+1) + " "
         print(bottomText)
 
-#no out-of-bounds check, only checking specific column
+#needs column index
     def checkValidMove(self, column):
-        if (column <=0 or
-            column >self.row):
+        if (column <0 or
+            column >=self.column):
             return False
-        if(self.board[0][column-1] == 0):
+        if(self.board[0][column] == 0):
             return True
         else:
             return False
 
+
+#needs column index
 #returns row of piece placement, -1 for no placement
-#might remove checkValidMove
     def placePiece(self, player, column):
         if (self.checkValidMove(column)):
             for rowPos in range(self.row-1, -1, -1):
-                if (self.board[rowPos][column-1] == 0):
-                    self.board[rowPos][column-1] = player
+                if (self.board[rowPos][column] == 0):
+                    self.board[rowPos][column] = player
                     return rowPos
         else:
             return -1
 
+#need row index
 #optimize this later
     def checkColumnWin(self, player, row):
         for pos in range(self.column - 3):
@@ -78,6 +110,7 @@ class BoardState:
                 return True
         return False
 
+#need column index
 #optimize this later
     def checkRowWin(self, player, column):
         for pos in range(self.row - 3):
@@ -106,8 +139,8 @@ class BoardState:
         return False
 
     def checkWin(self, player, row, column):
-        if(self.checkRowWin(player, row) or
-           self.checkColumnWin(player, column) or
+        if(self.checkRowWin(player, column) or
+           self.checkColumnWin(player, row) or
            self.checkDiagWin(player)):
             return True
         else:
@@ -124,117 +157,248 @@ class BoardState:
             return True
         else:
             return False
+
+#AI functions for "CustomConnect4.py"
+
+#add this later, will count potential partial rows for points
+    def pointCounter (self, board, player, lengthCount):
+         pass
+
+            
+#function returns a point value for the minimax algorithm
+    def scoreTheBoard (self, board, row, column, player):
+        if (board.checkDraw()):
+            return DRAW_VALUE
+            
+        elif (board.checkWin(player, row, column)):
+            return WIN_VALUE
+            
+    #change this to a scoring system later
+        else:
+            return 0
+
+
+    def miniMax(self, board, depth, alpha, beta, mPlayer, player, pWon):
+            
+        if (board.checkDraw() == True):
+                return DRAW_VALUE
+        if (pWon == True):
+            if (mPlayer == True):
+                return LOSE_VALUE
+            else:
+                return WIN_VALUE
+        if (depth == 0):
+            #Add scoring later
+                return DRAW_VALUE
+            #return board.scoreTheBoard(board, row, column, player)
+                    
+        if (mPlayer == True):
+            maxEval = MAX_EVAL
+            for column in range(self.column):
+                if (board.checkValidMove(column)):
+                    bCopy = board.boardCopy()
+                    row = bCopy.placePiece(player, column)
+                    if (bCopy.checkWin(player, row, column)):
+                        pWon = True
+                    evaluation = bCopy.miniMax(bCopy, depth - 1, alpha, beta, False, bCopy.otherPlayer(player), pWon)
+                    maxEval = max(maxEval, evaluation)
+                    alpha = max(alpha, evaluation)
+                    if (alpha >= beta):
+                        break
+            return maxEval
+        else:
+            minEval = MIN_EVAL
+            for column in range(self.column):
+                if (board.checkValidMove(column)):
+                    bCopy = board.boardCopy()
+                    row = bCopy.placePiece(player, column)
+                    if (bCopy.checkWin(player, row, column)):
+                        pWon = True
+                    evaluation = bCopy.miniMax(bCopy, depth - 1, alpha, beta, True, bCopy.otherPlayer(player), pWon)
+                    minEval = min(minEval, evaluation)
+                    beta = min(beta, evaluation)
+                    if (beta <= alpha):
+                        break
+            return minEval
+            
+    def bestMove(self, board, depth, player):
+        bestScore = MAX_EVAL
+        bestMove = None
+
+        for column in range(self.column):
+            if (board.checkValidMove(column)):
+                    bCopy = board.boardCopy()
+                    row = bCopy.placePiece(player, column)
+                    if (bCopy.checkWin(player, row, column)):
+                        pWon = True
+                    else:
+                        pWon = False
+                    score = bCopy.miniMax(bCopy, depth - 1, MAX_EVAL, MIN_EVAL, False, bCopy.otherPlayer(player), pWon)
+                    if (score > bestScore):
+                        bestScore = score
+                        bestMove = column
+                    elif (score == bestScore):
+#might need adjustment if MIN_WIDTH number is allowed to be 1 or smaller
+                        randomNumber = random.randint(0, 2)
+                        if (randomNumber == 0):
+                            bestScore = score
+                            bestMove = column
+        return bestMove
         
-#optimize not working       
-#    def checkDiagWin(self, player, row, column):
-#        if (row < 3):
-#            rowStart = 0
-#        else:
-#            rowStart = row - 3
-#            
-#        if (row > self.row -4):
-#            rowEnd = self.row -1
-#        else:
-#            rowEnd = row + 3
-#            
-#        if (column < 3):
-#            columnStart = 0
-#        else:
-#            columnStart = column - 3
-#            
-#        if (column > self.column -4):
-#            columnEnd = self.column -1
-#       else:
-#           columnEnd = column + 3
-#
-#        if(columnEnd - columnStart >= 3 and rowEnd - rowStart >= 3):
-#            for pos in range(rowStart, rowEnd - 3):
-#                for pos2 in range (columnStart, columnEnd - 2):
-#                    if (self.board[pos][pos2] == player and
-#                        self.board[pos + 1][pos2 + 1] == player and
-#                        self.board[pos + 2][pos2 + 2] == player and
-#                        self.board[pos + 3][pos2 + 3] == player):
-#                        return True
-#                for pos2 in range (columnEnd, columnStart + 2, -1):
-#                    if (self.board[pos][pos2] == player and
-#                        self.board[pos + 1][pos2 - 1] == player and
-#                        self.board[pos + 2][pos2 - 2] == player and
-#                        self.board[pos + 3][pos2 - 3] == player):
-#                        return True
-#                    
-#            return False
+    def aiPlacePiece(self, board, player):
         
+        row = board.bestMove(board, self.depth, player)
+        column = board.placePiece(player, row)
+        return [row, column]
+        
+        
+#maybe add 3rd player later
 def playerSwap(player):
     if (player == 1):
         return 2
     else:
         return 1
+    
+#maybe add 3rd player later
 def playerLabel(player):
     if (player == 1):
         return "X"
     else:
         return "O"
 
-#testBoard = [
-#    [2, 1, 1, 1],
-#    [1, 1, 2, 1],
-#    [1, 1, 2, 2],
-#    [1, 1, 2, 1],
-#]
-#board2 = BoardState(4,4, testBoard)
-#
-#board2.printBoard()
-#
-#print(board2.checkWin(1,1,3))
-#print(board2.checkDraw())
-
-
-#board = BoardState (4, 4)
-player = 2
-play = True
-draw = False
-width = int(input("Enter a width:"))
-height = int(input("Enter a height:"))
-board = BoardState(int(width), height)
-
-while (play == True):
+def invalidInput():
+    print("Invalid input.")
+    time.sleep(2)
     print("\n\n")
-    player = playerSwap(player)
-    invalidMove = True
-    while (invalidMove == True):
-        board.printBoard()
-        moveRowPos = -1
-        pLabel = playerLabel(player)
-        userInput = input("Player " + pLabel + " select move (type 1-" + str(board.getColumn()) + "):")
+
+def yesNoPrompt(userInput):
+    if(userInput.lower() != "n"):
+        return True
+    else:
+        return False
+    
+#parameter "height" is bool
+def inputBoardDimension(height):
+    if (height == True):
+        dimension = "height"
+        minimum = int(MIN_HEIGHT)
+        maximum = int(MAX_HEIGHT)
+                      
+    else:
+        dimension = "width"
+        minimum = int(MIN_WIDTH)
+        maximum = int(MAX_WIDTH)
+        
+    while (invalidInput):
         try:
-            moveRowPos = int(userInput)
-        except ValueError:
-            pass
-        if (board.checkValidMove(moveRowPos)):
-            columnPos = board.placePiece(player, moveRowPos)
-            invalidMove = False
+            userInput = int(input("Enter a board " + dimension + "(" + str(minimum) + "-" + str(maximum) + "):"))
 
-            if (board.checkWin(player, (moveRowPos -1), columnPos)):
-                play = False
-                
-            elif (columnPos == 0 and
-                board.checkDraw()):
-                play = False
-                draw = True
-                
+            if (userInput>= minimum and
+                userInput<= maximum):
+                return userInput
+            else:
+                invalidInput()
+        except:
+            invalidInput()
+    
+def promptPlayerAI (player):
+    invalidInput = True
+    while (invalidInput):
+        try:
+            userInput = input("Will player " + playerLabel(player) + " be AI controlled? (Y/N):")
+            return yesNoPrompt(userInput)
+        except:
+            invalidInput()
             
+#returns index of player's move
+def inputMove(board, pLabel):
+    invalidInput = True
+    while (invalidInput):
+        try:
+            board.printBoard()
+            userInput = input("Player " + pLabel + " select move (type 1-" + str(board.getColumn()) + "):")
+            moveRowPos = int(userInput) - 1
+            if (board.checkValidMove(moveRowPos)):
+                invalidInput = False
+                return userInput
+        except:
+            invalidInput()
+
+def inputPlayAgain():
+    invalidInput = True
+    while (invalidInput):
+        try:
+            userInput = input("Play again? (Y/N):")
+            return yesNoPrompt(userInput)
+        except:
+            invalidInput()
+            
+def inputSameRules():
+    invalidInput = True
+    while (invalidInput):
+        try:
+            userInput = input("Same Rules? (Y/N):")
+            return yesNoPrompt(userInput)
+        except:
+            invalidInput()
+
+sameRules = False
+keepPlaying = True
+while (keepPlaying == True):
+    
+    player = 1
+    
+    if (sameRules == False):
+        height = inputBoardDimension(True)
+        width = inputBoardDimension(False)
+        board = BoardState(height, width)
+        playerAI = [False, False]
+
+        #modify if making 3 person game
+        if (promptPlayerAI(1)):
+            playerAI[0] = True
+            
+        if (promptPlayerAI(2)):
+            playerAI[1] = True
+
+    else:
+        
+        sameRules = False
+        
+    board.clearBoard()   
+    draw = False    
+    play = True    
+    while (play == True):
+        if (playerAI[player - 1] == True):
+            pos = board.aiPlacePiece(board, player)
+            moveRowPos = pos[0]
+            columnPos = pos[1]
+                
         else:
-            print("Invalid Move.")
-            time.sleep(2)
-            print("\n\n")
+            moveRowPos = inputMove(playerLabel(player))
+            columnPos = board.placePiece(player, moveRowPos)              
 
+        if (board.checkWin(player, columnPos, moveRowPos)):
+            play = False
+                        
+        elif (columnPos == 0 and
+            board.checkDraw()):
+            play = False
+            draw = True
+            
+        print("\n\n")
+        board.printBoard()
+        player = playerSwap(player)
 
-board.printBoard()
-if(draw == True):
-    print("Draw!")
-else:
-    pLabel = playerLabel(player)
-    print("Player " + pLabel + " Wins!")
-
-
+    
+    if(draw == True):
+        print("Draw!")
+    else:
+        pLabel = playerLabel(playerSwap(player))
+        print("Player " + pLabel + " Wins!")
+        
+    keepPlaying = inputPlayAgain()
+    if(keepPlaying == True):
+        sameRules = inputSameRules()
+                          
     
